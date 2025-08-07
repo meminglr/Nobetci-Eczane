8112e2bbd809c660caf7fe119eb68f9506b8e51e
mhmt.eminguler <mhmt.eminguler@gmail.com>
2025-08-07 15:59:26 +0000

    some scroll phsyic to scroll experienece

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:myapp/home_controller.dart';
 import 'package:myapp/model/eczane_model.dart';
+import 'package:myapp/screens/exapndebele_map.dart';
 import 'package:myapp/screens/first_screen.dart';
 import 'package:myapp/screens/main_screen.dart';
+import 'package:myapp/screens/silver_screen.dart';
+import 'package:myapp/screens/sliver_screen2.dart';
 import 'package:myapp/services/eczane_service.dart';
 import 'package:myapp/widgets/companents.dart';
 
@@ -40,7 +43,7 @@ class _HomePageState extends State<HomePage> {
         if (isFirst) {
           return FirstScreen(companents: companents, controller: controller);
         } else {
-          return MainScrenn(
+          return SilverScreenView(
             controller: controller,
             eczaneService: eczaneService,
             companents: companents,
---
+++ lib/screens/exapndebele_map.dart
@@ -0,0 +1,279 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:myapp/home_controller.dart';
+import 'package:myapp/model/eczane_model.dart';
+import 'package:myapp/services/eczane_service.dart';
+import 'package:myapp/widgets/companents.dart';
+import 'package:myapp/widgets/example_list.dart';
+
+class ExpandebleScreen extends StatefulWidget {
+  final HomeController controller;
+  final EczaneService eczaneService;
+  final Companents companents;
+
+  const ExpandebleScreen({
+    super.key,
+    required this.controller,
+    required this.eczaneService,
+    required this.companents,
+  });
+  @override
+  State<ExpandebleScreen> createState() => _ExpandebleScreenState();
+}
+
+class _ExpandebleScreenState extends State<ExpandebleScreen> {
+  List<Data> dataList = DataList().dataList;
+  List<Data> eczaneListesi = [];
+  bool isLoading = true;
+  GoogleMapController? mapController;
+  Set<Marker> markers = {};
+
+  late ScrollController _scrollController;
+  double _scrollOffset = 0;
+  final double _minHeight = 100;
+  final double _maxHeight = 300;
+
+  /*
+  void _onScroll() {
+    double offset = _scrollController.offset;
+    double newHeight = (_maxHeight - offset).clamp(_minHeight, _maxHeight);
+    setState(() {
+      _mapHeight = newHeight;
+    });
+  } 
+
+*/
+
+  Future<void> _loadEczaneler() async {
+    eczaneListesi = [];
+    var result = await widget.eczaneService.getEczane(
+      widget.controller.normalizeToEnglish(widget.controller.secilenSehir!),
+      widget.controller.normalizeToEnglish(widget.controller.secilenIlce!),
+    );
+    eczaneListesi = result;
+
+    markers.clear();
+    for (var eczane in dataList) {
+      if (eczane.latitude != null && eczane.longitude != null) {
+        markers.add(
+          Marker(
+            markerId: MarkerId(
+              eczane.pharmacyName ?? Random().nextInt(1000).toString(),
+            ),
+            position: LatLng(eczane.latitude!, eczane.longitude!),
+            infoWindow: InfoWindow(title: eczane.pharmacyName),
+          ),
+        );
+      }
+    }
+
+    setState(() {
+      setState(() {
+        isLoading = false;
+      });
+    }); // ✅ Liste güncellenince UI yenilenir
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      setState(() {
+        _scrollOffset = _scrollController.offset;
+      });
+    });
+  }
+
+  double get _currentMapHeight {
+    return (_maxHeight - _scrollOffset).clamp(_minHeight, _maxHeight);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: widget.companents.floatingActionButton(
+        context: context,
+        companents: widget.companents,
+        controller: widget.controller,
+        onChanged: () {
+          _loadEczaneler();
+          setState(() {});
+        },
+      ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        scrolledUnderElevation: 0,
+        centerTitle: true,
+        title: Text(
+          "Nöbetçi Eczane",
+          style: TextStyle(
+            color: Colors.red,
+            fontWeight: FontWeight.w700,
+            fontSize: 30,
+          ),
+        ),
+        actions: [],
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TweenAnimationBuilder<double>(
+              tween: Tween<double>(begin: _maxHeight, end: _currentMapHeight),
+              duration: Duration(milliseconds: 200),
+              curve: Curves.fastOutSlowIn,
+              builder: (context, value, child) {
+                return SizedBox(
+                  height: value,
+                  width: double.infinity,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    child: GoogleMap(
+                      onMapCreated: (controller) => mapController = controller,
+                      initialCameraPosition: CameraPosition(
+                        target: LatLng(
+                          dataList.first.latitude!,
+                          dataList.first.longitude!,
+                        ),
+                        zoom: 13,
+                      ),
+                      markers: markers,
+                      myLocationEnabled: true,
+                      zoomControlsEnabled: false,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(child: eczaneListView(dataList)),
+        ],
+      ),
+    );
+  }
+
+  //***************************************************************// */
+
+  ListView eczaneListView(List<Data> dataList) {
+    return ListView.builder(
+      controller: _scrollController,
+      physics: BouncingScrollPhysics(),
+      itemCount: dataList.length,
+      itemBuilder: (context, index) {
+        var item = dataList[index];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            height: 100,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    borderRadius: BorderRadius.horizontal(
+                      left: Radius.circular(20),
+                    ),
+                    onTap: () {
+                      widget.eczaneService.openMap(
+                        item.latitude!,
+                        item.longitude!,
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.horizontal(
+                          left: Radius.circular(20),
+                          right: Radius.circular(5),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.map_outlined, color: Colors.blue[100]),
+                          Text(
+                            "Harita",
+                            style: TextStyle(color: Colors.blue[100]),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                    ),
+
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            item.pharmacyName!,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(item.address!, style: TextStyle(fontSize: 10)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    borderRadius: BorderRadius.horizontal(
+                      right: Radius.circular(20),
+                    ),
+                    onTap: () {
+                      widget.eczaneService.makePhoneCall(item.phone!);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.horizontal(
+                          right: Radius.circular(20),
+
+                          left: Radius.circular(5),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.call_outlined, color: Colors.green[100]),
+                          Text(
+                            "Ara",
+                            style: TextStyle(color: Colors.green[100]),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
